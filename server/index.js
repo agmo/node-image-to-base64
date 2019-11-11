e34cdf23cb270b12aa3567b097f2d9dde717aa22
agmo <mor.agnieszka@gmail.com>
2019-11-11 16:41:33 +0100

    Make file path safe (platform-agnostic)

--- server/index.js
+++ server/index.js
@@ -4,9 +4,10 @@ const app = express();
 app.listen(3000, () => console.log('Server is ready.'));
 
 const fs = require('fs');
+const path = require('path');
 app.get('/api/v1/photo', (req, res) => {
   const options = {encoding: 'base64'};
-  const stream = fs.createReadStream(__dirname + '/gratisography-paper-mache.jpg', options);
+  const stream = fs.createReadStream(path.join(__dirname, 'gratisography-paper-mache.jpg'), options);
 
   stream.pipe(res);
   stream.on('error', (err) => {
